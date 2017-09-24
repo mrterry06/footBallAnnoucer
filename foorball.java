b01d85ac39f08e96596cd8203aca00ef82ec70e2
Scott Terry <Scott Terry>
2017-09-24 16:52:57 -0700

    making changes

--- foorball.java
+++ foorball.java
@@ -1,9 +1,9 @@
 class TouchDownFor {
-	String player = new String("Tomilson");
-	String team = new String("Thunder");
 	public static void main(String args[]){
+		String team = new String("Thunder");
+		String player = new String("Tomilson");
 		for (int i = 50; i>= 0; i-= 5){
-			if ( i === 0) {
+			if ( i == 0) {
 				System.out.println("Touuchdown" + team + "!");
 			} else {
 				System.out.println(player + " is at the " + i + "!");
