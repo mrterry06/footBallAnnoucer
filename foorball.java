2fec929f706c80a21037f7f777245833f598e3f2
Scott Terry <Scott Terry>
2017-09-24 16:49:50 -0700

    added double quotes

--- foorball.java
+++ foorball.java
@@ -1,10 +1,10 @@
 class TouchDownFor {
-	String player = new String('Tomilson');
-	String team = new String('Thunder');
+	String player = new String("Tomilson");
+	String team = new String("Thunder");
 	public static void main(String args[]){
 		for (int i = 50; i>= 0; i-= 5){
 			if ( i === 0) {
-				System.out.println('Touuchdown' + team + "!");
+				System.out.println("Touuchdown" + team + "!");
 			} else {
 				System.out.println(player + " is at the " + i + "!");
 			}
